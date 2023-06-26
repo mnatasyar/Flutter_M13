2a6a60ee54f88a9ac1ddefaa642c12161c0f8634
M Natasya Ramadana <93390884+mnatasyar@users.noreply.github.com>
2023-06-26 16:05:04 +0700

    Update progress_indicator_widget.dart

--- progress_indicator_widget.dart
+++ progress_indicator_widget.dart
@@ -1,27 +1,25 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_m13/pertemuan13/pertemuan13_provider.dart';
 import 'package:provider/provider.dart';
 
 class ProgressIndicatorWidget extends StatelessWidget {
-  const ProgressIndicatorWidget({super.key});
+  const ProgressIndicatorWidget({Key? key});
 
   @override
   Widget build(BuildContext context) {
     final prov = Provider.of<Pertemuan13Provider>(context);
     return ElevatedButton(
-      style: ElevatedButton.styleFrom(minimumSize: Size(100, 90)),
+      style: ElevatedButton.styleFrom(minimumSize: const Size(100, 90)),
       onPressed: () {
         prov.mulaiMemanggang(prov.sliderValue.round());
       },
-      child: prov.sedangMemanggang == true
-          ? const CircularProgressIndicator(
-            color: Colors.white,
+      child: prov.sedangMemanggang
+          ? LinearProgressIndicator(
+              value: prov.progressValue, // Provide the progress value here
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              backgroundColor: Colors.grey,
             )
-          : const Text('Panggang'));
+          : const Text('Panggang'),
+    );
   }
 }
-
-Ubah circularProgressIndicator menjadi tipe determine indicator sehingga 
-menampilkan lama proses.
